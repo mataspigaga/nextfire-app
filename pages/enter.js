8457ff48ed931771b4131132f0d528763c6cdf1b
Matas Pigaga <tech@matas.io>
2021-10-21 15:23:18 -0400

    added navbar

--- pages/enter.js
+++ pages/enter.js
@@ -4,16 +4,6 @@ export default function EnterPage({}) {
   return (
     <main>
       <h1>Sign Up</h1>
-      <ul>
-        <Link href="/">Home Page</Link>
-        <br />
-        <Link href="/admin">Admin Page</Link>
-        <br />
-        <Link href="/admin">User Page</Link>
-        <br />
-        <Link href="/admin">Post Page</Link>
-        <br />
-      </ul>
     </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1>Totally not a Dev.to clone</h1>
-      <ul>
-        <Link href="/enter">Sign Up Page</Link>
-        <br />
-        <Link href="/admin">Admin Page</Link>
-        <br />
-        <Link href="/admin">User Page</Link>
-        <br />
-        <Link href="/admin">Post Page</Link>
-        <br />
-      </ul>
     </main>
   );
 }
