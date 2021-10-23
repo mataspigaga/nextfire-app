135b8c3836b0076e271f4ec568db7ce0a2b8e45a
Matas Pigaga <tech@matas.io>
2021-10-23 13:07:50 -0400

    added sign in page / Google auth / user page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
 
 // Top navbar
 export default function Navbar() {
-  const user = false;
-  const username = false;
+  const { user, username } = useContext(UserContext);
 
   return (
     <nav className="navbar">
